693cdda46fb4c666cc0a1cda526e865396aec781
Vanese <vanesebuchanan@gmail.com>
2022-10-29 12:40:57 -0400

    setting up file structure

---
+++ notsureyet.js
@@ -0,0 +1,8 @@
+// view all departments has table showing department names and department ids
+// view all roles has table showing job title, role id, the department that role belongs to and the salary for that role
+// view all employees has table showing employee data (employee ids, first names, last names, job titles, departments 
+// salaries, and managers that the employees report to)
+// choose to add a department has prompt to enter the name of the department and department is added to the database
+// choose to add a role has prompt to enter name, salary, and department for the role and role is added to the database
+// choose to add an employee has prompt to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// update an employee role has prompt to select an employee to update and their new role and this information is updated in the database 
---
+++ server.js
@@ -0,0 +1,17 @@
+//starting cmd line application has selectable options:
+// view all departments
+// view all roles
+// view all employees
+// add a department 
+// add a role 
+// add an employee 
+// update an employee role
+
+
+// You’ll need to use the [MySQL2 package](https://www.npmjs.com/package/mysql2) to connect to your MySQL database and perform queries 
+// [Inquirer package](https://www.npmjs.com/package/inquirer) to interact with the user via the command line 
+// [console.table package](https://www.npmjs.com/package/console.table) to print MySQL rows to the console.
+
+// Make your queries asynchronous. MySQL2 exposes a `.promise()` function on Connections to upgrade 
+// an existing non-Promise connection to use Promises. Rrefer to the [npm documentation on MySQL2](https://www.npmjs.com/package/mysql2).
+
