9c54681197ffc1e139dcfbd74fc9ca4d45be8ab3
Vanese <vanesebuchanan@gmail.com>
2022-10-29 14:18:34 -0400

    create database and tables in schema

---
+++ db/schema.sql
@@ -0,0 +1,45 @@
+-- [Database schema includes tables labeled “employee,” role,” and “department.”](./Assets/12-sql-homework-demo-01.png)
+-- // As the image illustrates, your schema should contain the following three tables:
+
+
+DROP DATABASE IF EXISTS employee_directory_db;
+CREATE DATABASE employee_directory_db;
+
+
+USE employee_directory_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id),
+  REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id),
+  REFERENCES role(id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
