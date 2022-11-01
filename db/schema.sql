291eb5dddb04a8364daced41b67203006658c20c
Vanese <vanesebuchanan@gmail.com>
2022-11-01 18:36:37 -0400

    tables showing now

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
 -- [Database schema includes tables labeled “employee,” role,” and “department.”](./Assets/12-sql-homework-demo-01.png)
 
-DROP DATABASE IF EXISTS employee_directory_db;
-CREATE DATABASE employee_directory_db;
+-- DROP DATABASE IF EXISTS employee_directory_db;
+-- CREATE DATABASE employee_directory_db;
 
 
 USE employee_directory_db;
+
+DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -15,9 +17,9 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id)
-  REFERENCES departments(id)
+  department_id INT
+  FOREIGN KEY (department_id),
+  REFERENCES departments(id),
 );
 
 CREATE TABLE employee (
@@ -25,9 +27,9 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id)
-  REFERENCES roles(id)
+  manager_id INT
+  FOREIGN KEY (role_id),
+  REFERENCES roles(id),
 );
 
 
